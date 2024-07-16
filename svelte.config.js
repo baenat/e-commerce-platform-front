baad68114d3b3983912d1447e564e7db4e506512
Baenat <bae.julian10@gmail.com>
2024-07-16 01:20:33 -0500

    Config svelte js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ status, path}) => {
+        
+        if (status === 404 || status === 405) {
+          return;
+        }
+
+        throw new Error(`${status} ${path}`);
+      },
+      handleMissingId: 'ignore'
+    }
   },
-  preprocess: vitePreprocess()
+
+  preprocess: vitePreprocess(),
+
 };
 export default config;
